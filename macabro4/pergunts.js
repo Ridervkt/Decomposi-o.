6aed0aa1712e3e95abd37651b2eb175f1409f737
Ridervkt <vini.kenzo.to@gmail.com>
2024-11-15 22:25:54 -0300

    Add files via upload

---
+++ macabro4/pergunts.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Antropologia forense',
+    'Quanto tempo demora para um corpo se decompor ao ar livre?',
+    'Entre duas e seis semanas para'
+)
+
+criaCartao(
+    'Antropologia forense',
+    'Quanto tempo demora para um corpo se decompor sob a terra?',
+    'Entre nove meses a cinco anos. '
+)
+
+criaCartao(
+    'Antropologia forense',
+    'Quanto tempo demora para um corpo se decompor no fundo do mar?',
+    'Até dez anos'
+)
+
+criaCartao(
+    'Antropologia forense',
+    'Quanto tempo demora para um corpo se decompor em um pântano?',
+    'Indefinido'
+)
